fd1544ada5f74d0987912a20cf74992ec01953d4
Stephan Septh Schreiber <septh@sfr.fr>
2019-07-08 09:58:55 +0200

    Commit

---
+++ bundle-js.js
@@ -0,0 +1,13 @@
+const mult = (x, y = Test.defaultFactor) => x * y;
+
+class Test {
+    mul() {
+        return mult(2)
+    }
+}
+Test.defaultFactor = 1;
+
+const t = new Test();
+console.log(t.mul());
+
+export { Test };
---
+++ bundle-official.js
@@ -0,0 +1,30 @@
+var Test = /** @class */ (function () {
+    function Test() {
+    }
+    Test.prototype.mul = function () {
+        return mult(2);
+    };
+    Test.defaultFactor = 1;
+    return Test;
+}());
+var t = new Test();
+console.log(t.mul());
+
+var mult = function (x, y) {
+    if (y === void 0) { y = Test.defaultFactor; }
+    return x * y;
+};
+
+var Test$1 = /** @class */ (function () {
+    function Test() {
+    }
+    Test.prototype.mul = function () {
+        return mult(2);
+    };
+    Test.defaultFactor = 1;
+    return Test;
+}());
+var t$1 = new Test$1();
+console.log(t$1.mul());
+
+export { Test$1 as Test };
